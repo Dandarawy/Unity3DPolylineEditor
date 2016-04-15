89b573134cac2f7df854efe227db2fd3780d1ab8
Dandarawy <abdulla.aldandarawy@gmail.com>
2016-04-15 19:15:59 +0200

    update the Polyline script to be initialzed with two points by default

--- Assets/Polyline Editor/Scripts/Polyline.cs
+++ Assets/Polyline Editor/Scripts/Polyline.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 public class Polyline : MonoBehaviour {
-    public List<Vector3> nodes = new List<Vector3>();
+    public List<Vector3> nodes = new List<Vector3>(new Vector3[] { new Vector3(-3, 0, 0), new Vector3(3, 0, 0) });
 
     // Use this for initialization
     void Start () {
